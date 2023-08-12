04b7f48df1d04e59727e750d313a70300a25b440
Philip Obiorah <philipobiorah@Philips-MacBook-Pro.local>
2023-08-12 23:14:32 +0100

    is_power_of.py

---
+++ is_power_of.py
@@ -0,0 +1,14 @@
+def is_power_of(number, base):
+    print(f"calling...is_power_of({number},{base})")
+    #Base case: when nubmer is smaller than base.
+    if number < base:
+        # If number is equal to 1, its a power (base**1)
+        return number == 1
+    
+    # Recusive case: keep dividing number by base.
+    return is_power_of(number/base, base)
+
+print(is_power_of(8,2)) # Should be True
+print(is_power_of(64,4)) # Should be True
+print(is_power_of(70,10)) # Should be False
+        
