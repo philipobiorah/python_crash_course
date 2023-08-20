7a3fa60a30b78fd7609352cadf351a157f5b9887
Philip Obiorah <philip.obiorah@outlook.com>
2023-08-20 09:49:22 +0100

    nametag.py

---
+++ nametag.py
@@ -0,0 +1,11 @@
+def nametag(first_name, last_name):
+    return("{}. {[0]}".format(first_name,last_name))
+
+
+
+print(nametag("Jane", "Smith")) 
+# Should display "Jane S." 
+print(nametag("Francesco", "Rinaldi")) 
+# Should display "Francesco R." 
+print(nametag("Jean-Luc", "Grand-Pierre")) 
+# Should display "Jean-Luc G.
