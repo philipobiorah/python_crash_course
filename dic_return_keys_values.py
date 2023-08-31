27ebeb150eedf6db05faaf8040d5fed0656d9fc4
Philip Obiorah <philip.obiorah@outlook.com>
2023-08-31 18:23:01 +0100

    dic_return_key_values.py

---
+++ dic_return_keys_values.py
@@ -0,0 +1,27 @@
+def network(servers):
+
+    # A string variable is initialize to hold the result
+    result = ""
+
+    # For each "hostname" (key) and "IP address" (value) in the "server" dictionary items...
+    for hostname, IP_address in servers.items():
+        # A string identifying the hostname and IP address for each server is added
+        # to the "result" variable. The string .format() function and is used to plug
+        # the hostname and IP_address variables into the designated {} placeholders
+        # within the string.
+        result += "The IP address of the {} server is {}".format(hostname, IP_address) + "\n"
+
+    # Return the "result" variable string.
+    return result
+
+
+
+
+# Call the "network" function with the dictionary. 
+print(network({"Domain Name Server":"8.8.8.8", "Gateway Server":"192.168.1.1", "Print Server":"192.168.1.33", "Mail Server":"192.168.1.190"}))
+
+# Should print:
+# The IP address of the Domain Name Server server is 8.8.8.8
+# The IP address of the Gateway Server server is 192.168.1.1
+# The IP address of the Print Server server is 192.168.1.33
+# The IP address of the Mail Server server is 192.168.1.190
