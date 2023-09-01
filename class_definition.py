efd44b28be493e2ba7a148b7bacd25aba276757c
Philip Obiorah <philip.obiorah@outlook.com>
2023-09-01 22:59:41 +0100

    server_event_logger

---
+++ class_definition.py
@@ -0,0 +1,17 @@
+class Apple:
+    color = ""
+    flavour = ""
+
+
+jonagold = Apple()
+
+jonagold.color = "red"
+jonagold.flavour = "sweet"
+
+
+print(jonagold.color.upper())
+print(jonagold.flavour)
+
+
+golden = Apple()
+golden.color = "Yellow"
---
+++ server_event_logger_project.py
@@ -0,0 +1,10 @@
+class Event:
+    def __init__(self, event_date, event_type, machine_name, user):
+        self.date = event_date
+        self.type = event_type
+        self.machine = machine_name
+        self.user = user
+        
+
+
+        
